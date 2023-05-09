6d36d3b485212a2f3cfc6b94aea366912bc3006f
Ihor Perkovskyi <igor.v.perkovsky@gmail.com>
2023-05-09 19:23:19 -0400

    NtpDateTimeProvider has been refactored

--- lib/DateTimeProvider/NtpDateTimeProvider.hpp
+++ lib/DateTimeProvider/NtpDateTimeProvider.hpp
@@ -1,9 +1,3 @@
-//////////////////////////////////////////////////////////////
-//                                                          //
-//     Doesn't work correctly. Refactoring is required.     //    
-//                                                          //
-//////////////////////////////////////////////////////////////
-
 #pragma once
 
 #include <Arduino.h>
@@ -28,7 +22,7 @@ public:
     String getTimestamp() override {
         tm timeInfo = getTimeInfo();
         char timestamp[20];
-        sprintf(timestamp, "20%02d-%02d-%02d %02d:%02d:%02d", timeInfo.tm_year, timeInfo.tm_mon, timeInfo.tm_mday, timeInfo.tm_hour, timeInfo.tm_min, timeInfo.tm_sec);
+        strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", &timeInfo);
         return String(timestamp);
     }
 };
